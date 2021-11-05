ea8795470d4dbb88b9ceac8370ea5f30c12a3f8f
Damion Mul <42682912+Pmobilee@users.noreply.github.com>
2021-11-05 09:29:22 +0100

    Motion test

--- socialroboticshub-connectors/python/record_and_play_motion_example.py
+++ socialroboticshub-connectors/python/record_and_play_motion_example.py
@@ -20,20 +20,20 @@ class Example:
         self.action_runner.run_loaded_actions()
 
         self.action_runner.run_waiting_action('set_stiffness', joints, 0)
-        self.action_runner.load_waiting_action('say', 'You can now move my right arm.')
+        self.action_runner.load_waiting_action('say', 'Move right arm please')
         self.action_runner.load_waiting_action('start_record_motion', joints)
         self.action_runner.run_loaded_actions()
         sleep(5)
         self.action_runner.load_waiting_action('stop_record_motion', additional_callback=self.retrieve_recorded_motion)
-        self.action_runner.load_waiting_action('say', 'Finished recording')
+        self.action_runner.load_waiting_action('say', 'Done')
         self.action_runner.run_loaded_actions()
 
-        self.action_runner.run_waiting_action('set_stiffness', joints, 100)
-        if self.motion:
-            self.action_runner.run_waiting_action('say', 'I will now replay your movement')
-            self.action_runner.run_waiting_action('play_motion', self.motion)
-        else:
-            self.action_runner.run_waiting_action('say', 'Something went wrong.')
+        # self.action_runner.run_waiting_action('set_stiffness', joints, 100)
+        # if self.motion:
+        #     self.action_runner.run_waiting_action('say', 'I will now replay your movement')
+        #     self.action_runner.run_waiting_action('play_motion', self.motion)
+        # else:
+        #     self.action_runner.run_waiting_action('say', 'Something went wrong.')
 
         self.action_runner.run_waiting_action('rest')
         self.sic.stop()
@@ -42,5 +42,7 @@ class Example:
         self.motion = motion
 
 
+
 example = Example('127.0.0.1')
 example.run()
+print(type(example.motion))
