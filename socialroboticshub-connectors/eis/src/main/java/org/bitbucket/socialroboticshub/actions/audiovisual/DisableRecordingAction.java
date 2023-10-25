20c491e7de2ab2d14308ace29ada194c7ac729f2
Pmobilee <muldamion@gmail.com>
2023-10-25 14:31:19 +0200

    Delete socialroboticshub-connectors directory

--- socialroboticshub-connectors/eis/src/main/java/org/bitbucket/socialroboticshub/actions/audiovisual/DisableRecordingAction.java
+++
@@ -1,9 +0,0 @@
-package org.bitbucket.socialroboticshub.actions.audiovisual;
-
-public class DisableRecordingAction extends DialogflowRecordingAction {
-	public final static String NAME = "disableRecording";
-
-	public DisableRecordingAction() {
-		super(false);
-	}
-}
